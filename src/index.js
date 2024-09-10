2ac39899527f0d3539b2411428749d018652d62a
Snoke19 <alex.developer27@outlook.com>
2024-09-10 23:24:06 +0300

    removed unnecessary css file

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './assets/index.css';
-import 'highlight.js/styles/github.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
